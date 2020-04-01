4fa31f5fd8d36f8a06cd5522aec4955c3abd1633
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-04-01 15:49:34 -0500

    println replace by logger

--- hand_wash/src/main/java/cs_utec/cs2901/App.java
+++ hand_wash/src/main/java/cs_utec/cs2901/App.java
@@ -1,15 +1,17 @@
 package cs_utec.cs2901;
 
-/**
- * Hello world!
- *
- */
+import org.apache.log4j.Logger;
+
 public class App 
 {
+    final static Logger logger = Logger.getLogger(App.class);
+
     public static void main( String[] args )
     {
-        for (int i = 0; i < 100; i++) {
-            System.out.println("Me debo lavar las manos por 20 segundos");
+        for (int i = 0; i < 100; i++)
+        {
+            //System.out.println("Me debo lavar las manos por 20 segundos");
+            logger.info("Me debo lavar las manos por 20 segundos");
         }
     }
 }
